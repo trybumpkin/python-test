af6c3e7712310ca05f2c36fc27285c47d4335ee6
Anas Al-Sharif <39890019+anasalsharif@users.noreply.github.com>
2026-06-20 21:59:48 +0300

    Remove farewell API
    Breaking test PR for Bumpkin validation.

--- src/python_test/__init__.py
+++ src/python_test/__init__.py
@@ -1,7 +1,6 @@
 from .api import DEFAULT_GREETING, greet, greet_many, greet_pair
 from .catalog import available_greetings
 from .formal import greet_formally
-from .farewell import DEFAULT_FAREWELL, farewell
 
 __all__ = [
     "DEFAULT_GREETING",
@@ -10,6 +9,4 @@ __all__ = [
     "greet_pair",
     "available_greetings",
     "greet_formally",
-    "DEFAULT_FAREWELL",
-    "farewell",
 ]
